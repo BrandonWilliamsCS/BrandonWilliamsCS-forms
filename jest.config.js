c7eb34a640ac9500a4fc6f686e010243e780dabd
Brandon Williams <Brandon.Williams@blueharborsolutions.com>
2021-05-04 12:40:23 -0400

    cleaning some inappropriately broad files

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   transform: { "^.+\\.tsx?$": "ts-jest" },
-  testRegex: "\\.(test|spec)\\.(ts|tsx)$",
+  testRegex: "./src/.*\\.(test|spec)\\.(ts|tsx)$",
   moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json"],
   setupFilesAfterEnv: ["<rootDir>/src/setupTests.ts"],
   moduleDirectories: ["node_modules", "src"],
